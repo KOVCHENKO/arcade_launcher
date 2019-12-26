61753cbb25f0beb7f18e8265f048de1dde248467
KOVCHENKO <il-kow@mail.ru>
2019-12-26 14:43:35 +0300

    upd(Main.java) full screen

--- src/arcade/Main.java
+++ src/arcade/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
@@ -12,7 +13,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Start page");
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(new Scene(root, 500, 275));
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
