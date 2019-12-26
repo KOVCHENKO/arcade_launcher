57e2de8f57ae04c65be640d8008ea4e1d6ed9bd4
KOVCHENKO <il-kow@mail.ru>
2019-12-26 14:38:25 +0300

    feat(alerting window)

--- src/arcade/Main.java
+++ src/arcade/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Start page");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 500, 275));
         primaryStage.show();
     }
 
--- src/arcade/controllers/MainController.java
+++ src/arcade/controllers/MainController.java
@@ -1,5 +1,20 @@
 package arcade.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+
+
 public class MainController {
 
+    @FXML
+    public void start(ActionEvent event) {
+        System.out.println("it is starting");
+        showAlert("START!", "IT IS STARTING", Alert.AlertType.INFORMATION);
+    }
+
+    private void showAlert(String message, String title, Alert.AlertType type) {
+        Alert alert = new Alert(type);
+        alert.showAndWait();
+    }
 }
